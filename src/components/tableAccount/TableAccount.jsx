32e9c7824fa07454c7ebacc8a23d296c80f0a78c
zulfa nurhuda <zulfaanurhuda28@gmail.com>
2024-04-21 19:26:19 +0700

    fixing button copy & delete

--- src/components/tableAccount/TableAccount.jsx
+++ src/components/tableAccount/TableAccount.jsx
@@ -4,6 +4,7 @@ import {
   MdOutlineContentCopy,
   MdOutlineDeleteForever,
 } from "react-icons/md"
+import { HiOutlineDotsHorizontal } from "react-icons/hi"
 
 const TableAccount = () => {
   return (
@@ -24,7 +25,7 @@ const TableAccount = () => {
             placeholder="NO PEMBAYARAN:..."
           />
         </div>
-        <div className="h-16 text-center px-1  text-white relative border border-b-black">
+        <div className="h-24 text-center px-1  text-white relative border border-b-black">
           <input
             type="text"
             className="h-full bg-transparent ms-7 ps-2 w-[85%] placeholder:text-center text-black"
@@ -36,33 +37,43 @@ const TableAccount = () => {
           </div>
         </div>
         <div className="flex px-1 items-center text-white  border border-l-black border-b-black">
-          <MdOutlineContentCopy className="text-[#128DFF] text-2xl" />
-          <input
-            type="text"
-            className=" w-full h-full bg-transparent ps-4 text-black"
-            placeholder="KETIK DISINI..."
-          />
-          <MdOutlineDeleteForever className="text-[#FF0000] text-3xl" />
+          <div className="grid grid-rows-[2fr_1fr]">
+            <input
+              type="text"
+              className=" w-full h-full bg-transparent ps-4 text-black"
+              placeholder="KETIK DISINI..."
+            />
+            <div className="flex items-center justify-between">
+              <MdOutlineContentCopy className="text-[#128DFF] text-2xl" />
+              <MdOutlineDeleteForever className="text-[#FF0000] text-3xl" />
+            </div>
+          </div>
         </div>
-        <div className="h-16 text-center px-1  text-white relative border border-b-black">
+        <div className="h-24 text-center px-1  text-white relative border border-b-black">
           <input
             type="text"
             className="h-full bg-transparent ms-7 ps-2 w-[85%] text-black placeholder:text-center"
             placeholder="KETIK DISINI..."
           />
-          <div className="text-black absolute flex flex-col items-center w-8 left-0 top-0 pt-3 border border-r-black h-full">
-            <span>10.</span>
-            <MdDoneOutline className="text-[#148EFF]" />
+          <div className="text-black absolute flex flex-col items-center w-8 left-0 top-0 pt-6 border border-r-black h-full">
+            <MdDoneOutline className="text-[#B2ADAD]" />
+            <span className="text-[#B2ADAD]">10.</span>
+            <HiOutlineDotsHorizontal className="text-[#B2ADAD]" />
           </div>
         </div>
-        <div className="flex px-1 items-center text-white  border border-l-black border-b-black">
-          <MdOutlineContentCopy className="text-[#128DFF] text-2xl" />
-          <input
-            type="text"
-            className="w-full h-full bg-transparent ps-4 text-black "
-            placeholder="KETIK DISINI..."
-          />
-          <MdOutlineDeleteForever className="text-[#FF0000] text-3xl" />
+        <div className="h-24 px-1 items-center text-white  border border-l-black border-b-black">
+          <div className="grid grid-rows-[2fr_1fr]">
+            <input
+              type="text"
+              className="w-full  bg-transparent ps-4 text-black "
+              placeholder="KETIK DISINI..."
+            />
+            <div className="flex items-center justify-between">
+              <MdOutlineContentCopy className="text-[#128DFF] text-2xl" />
+
+              <MdOutlineDeleteForever className="text-[#FF0000] text-3xl" />
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-[96vw] mx-auto py-2 bg-[#26BC00] rounded-b-xl">
--- src/components/timedeadline/TimeDeadLine.jsx
+++ src/components/timedeadline/TimeDeadLine.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { FaRegPlayCircle } from "react-icons/fa"
 import CatatanJobs from "../catatanjobs/CatatanJobs"
 
 const TimeDeadLine = () => {
   const [isStart, setIsStart] = useState(false)
+
+  const [day, setDay] = useState("")
+  const [hours, setHours] = useState("")
+  const [minutes, setMinutes] = useState("")
+  const [second, setSecond] = useState("")
+  const setDayRef = useRef()
+  const setHoursRef = useRef()
+  const setMinutesRef = useRef()
+  const setSecondRef = useRef()
 
   return (
     <div className=" w-full relative">
@@ -12,18 +21,24 @@ const TimeDeadLine = () => {
       </h1>
       <div className=" absolute  top-7 left-3 right-3 bg-[#F2F8FF] rounded-md">
         <div className="grid grid-cols-4 justify-center text-center mx-auto gap-x-2 -gap-y-5 px-10 mt-10">
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl">0</h1>
-          <h1 className="text-3xl">0</h1>
-          <h1 className="text-3xl">0</h1>
-          <h1 className="text-3xl">0</h1>
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl font-extralight text-[#959494]">0</h1>
-          <h1 className="text-3xl font-extralight text-[#1d1717]">0</h1>
+          {/* <h1 className="text-3xl font-extralight text-[#959494]">30</h1>
+          <h1 className="text-3xl font-extralight text-[#959494]">23</h1>
+          <h1 className="text-3xl font-extralight text-[#959494]">59</h1>
+          <h1 className="text-3xl font-extralight text-[#959494]">59</h1> */}
+
+          <div className="flex flex-col h-28  overflow-y-scroll">
+            <h1 className="text-3xl font-extralight text-[#959494]">30</h1>
+            <h1 className="text-3xl font-extralight text-[#959494]">30</h1>
+            <h1 className="text-3xl" ref={setDayRef}>
+              00
+            </h1>
+            <h1 className="text-3xl font-extralight text-[#959494]">01</h1>
+            <h1 className="text-3xl font-extralight text-[#959494]">01</h1>
+          </div>
+          <h1 className="text-3xl">00</h1>
+          <h1 className="text-3xl">00</h1>
+          <h1 className="text-3xl">00</h1>
+
           <h1 className="text-sm font-extralight text-[#1d1717]">Hari</h1>
           <h1 className="text-sm font-extralight text-[#1d1717]">Jam</h1>
           <h1 className="text-sm font-extralight text-[#1d1717]">Menit</h1>
