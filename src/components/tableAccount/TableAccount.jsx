b2dc37c0027de3d32e13a8c39fb95196aacb1437
zulfa nurhuda <zulfaanurhuda28@gmail.com>
2024-04-24 19:39:29 +0700

    fixing number table

--- src/components/tableAccount/TableAccount.jsx
+++ src/components/tableAccount/TableAccount.jsx
@@ -8,8 +8,19 @@ import { HiOutlineDotsHorizontal } from "react-icons/hi"
 
 const TableAccount = () => {
   const [number, setNumber] = useState("")
+  const [numberTable, setNumberTable] = useState("")
 
   const tableRef = useRef()
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setNumberTable(number)
+    }, 500)
+
+    return () => {
+      clearTimeout(time)
+    }
+  }, [number])
 
   return (
     <>
@@ -46,7 +57,39 @@ const TableAccount = () => {
           </div>
         </div>
 
-        {number.length === 0 ? (
+        {/* <div className="h-24 text-center px-1  text-white relative border border-b-black">
+          <input
+            type="text"
+            className="h-full bg-transparent ms-7 ps-2 w-[85%] text-black placeholder:text-center"
+            placeholder="KETIK DISINI..."
+          />
+          <div className="text-black absolute flex flex-col items-center w-8 left-0 top-0 pt-6 border border-r-black h-full">
+            <MdDoneOutline className="text-[#B2ADAD]" />
+            <input
+              type="text"
+              placeholder="10"
+              className="placeholder:text-[#B2ADAD] text-center w-full"
+              value={number}
+              onChange={handleChange}
+            />
+            <HiOutlineDotsHorizontal className="placeholder:text-[#B2ADAD]" />
+          </div>
+        </div>
+        <div className="h-24 px-1 items-center text-white  border border-l-black border-b-black">
+          <div className="grid grid-rows-[2fr_1fr]">
+            <input
+              type="text"
+              className="w-full  bg-transparent ps-4 text-black "
+              placeholder="KETIK DISINI..."
+            />
+            <div className="flex items-center justify-between">
+              <MdOutlineContentCopy className="text-[#128DFF] text-2xl" />
+              <MdOutlineDeleteForever className="text-[#FF0000] text-3xl" />
+            </div>
+          </div>
+        </div> */}
+
+        {numberTable.length === 0 ? (
           <>
             <div className="h-24 text-center px-1  text-white relative border border-b-black">
               <input
@@ -81,7 +124,7 @@ const TableAccount = () => {
             </div>
           </>
         ) : (
-          new Array(parseInt(number)).fill("")?.map((_, i) => {
+          new Array(parseInt(numberTable))?.fill("")?.map((_, i) => {
             return (
               <>
                 <div className="h-24 text-center px-1  text-white relative border border-b-black">
@@ -112,14 +155,8 @@ const TableAccount = () => {
             )
           })
         )}
-
-        {/* {
-          new Array(parseInt(number)).fill("")?.map((_,i)=>{
-           
-          })
-        } */}
       </div>
-      <div className="w-[96vw] mx-auto py-2 bg-[#26BC00] rounded-b-xl">
+      <div className="w-[96vw] mx-auto py-2 mb-4 bg-[#26BC00] rounded-b-xl">
         <div className="flex justify-end items-center gap-x-2 text-white pe-2">
           <span>SALIN SEMUA</span>
           <MdOutlineContentCopy />
